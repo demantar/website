ad4f5425dd68b7713054e67af1479f944ba878b3
demantar <BeVM01@rvkskolar.is>
2018-07-26 17:53:42 +0000

    r

--- redo/debug/script.js
+++ redo/debug/script.js
@@ -1,8 +1,9 @@
 function setup() {
   createCanvas(400, 400);
+  let img = createImage(28, 28);
   loadPixels();
-  console.log("pixels: " + pixels);
-  console.log("length: " + pixels.length);
-  console.log("width * heigt * 4: " + width * height * 4);
+  console.log("pixels: " + img.pixels);
+  console.log("length: " + img.pixels.length);
+  console.log("width * heigt * 4: " + img.width * img.height * 4);
   updatePixels();
 }
